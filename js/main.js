9f56103b82765991a69a49c708c848d6a0e59233
Darth Roman <chouchane.louai20@gmail.com>
2023-03-17 03:53:41 +0100

    JS: deleteTodo, disabled todo on click and delete on double click /n CSS: disabled the todo style

--- js/main.js
+++ js/main.js
@@ -36,24 +36,30 @@ const saveTaskToLocalStorage = (todo) => {
     if (tasks) localStorage.setItem('tasks', JSON.stringify([...tasks, todo]))
 }
 
+
+
+
 // DOM manipulation
 const addTaskToDOM = () => {
     const todoListContainer = document.getElementById('todo-list')
     const data = formData()
 
+    console.log(data);
+
     todoListContainer.innerHTML += `
-    <div class="todo">
+    <div class="todo" data-key="${data.id}" onclick="${() => deleteTodo(data.id)}">
         <div class="content">
             <p>${data.task}</p>
         </div>
         <div class="status">
             <small class="tag tag-success">${data.stat}</small>
-            <button class="btn btn-red">Delete</button>
+            <button class="btn btn-red" onclick="${() => deleteTodo(data.id)}">Delete</button>
         </div>
     </div>
     `
     document.getElementById('todo-field').value = ''
     saveTaskToLocalStorage(data)
+    location.reload()
 }
 
 // From localStorage to DOM
@@ -63,13 +69,13 @@ const integrateFetchedDataToDOM = () => {
     if(tasks.length===0) todoListContainer.innerHTML = `<p>Nothing found</p>`
     else tasks.map(task => 
         todoListContainer.innerHTML += `
-        <div class="todo">
+        <div class="todo" data-key="${task.id}">
             <div class="content">
                 <p>${task.task}</p>
             </div>
             <div class="status">
                 <small class="tag tag-success">${task.stat}</small>
-                <button class="btn btn-red">Delete</button>
+                <button class="btn btn-red" onclick="${()=>deleteTodo(task.id)}">Delete</button>
             </div>
         </div>
         
@@ -83,7 +89,37 @@ const laodingThePageEvents = () => {
     integrateFetchedDataToDOM()
 }
 
-//integratFetchedDataToDOM()
+
+
+
 // Events listners
 addBtn.addEventListener('click', addTaskToDOM)
 window.addEventListener('load', laodingThePageEvents)
+
+
+
+
+const deleteTodo = (todoID) => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'))
+
+    const newTaskArray = tasks.filter(task => task.id != todoID)
+    console.log(newTaskArray);
+
+    localStorage.setItem('tasks', JSON.stringify(newTaskArray))
+    location.reload()
+
+}
+
+document.addEventListener('click', (e)=>{
+    const target = e.target
+    if(target.tagName === 'DIV'){
+        // console.log(target);
+        target.classList.toggle('todo-disabled')
+        target.children[1].children[0].textContent = 'Done/Doing'
+    }
+})
+
+document.addEventListener('dblclick', (e)=>{
+    const target = e.target
+    if(target.classList.value === 'todo' || target.classList.value === 'todo todo-disabled') deleteTodo(target.dataset.key)
+})
